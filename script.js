2c5d755265f9c1d4f3c8d62321bc5b9b53c9f949
Rajya-V <rajya4107@gmail.com>
2024-11-16 12:25:53 +0530

    initial commit2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const cards = [
-    '*','*','#','#','@','@','&','&','!','!','$','$','^','^','~','~'
+    'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'
 ];
+
+let flippedCards = []; // To track flipped cards
 
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -17,26 +19,41 @@ function createBoard() {
         const cardElement = document.createElement('div');
         cardElement.classList.add('card');
         cardElement.dataset.card = card;
+        cardElement.innerText = '';  // No text initially
         gameContainer.appendChild(cardElement);
     });
 }
 
 function flipCard(event) {
-    event.preventDefault();
     const clickedCard = event.target;
-    clickedCard.classList.add('flipped');
-    if (document.querySelector('.flipped')) {
-        const firstCard = document.querySelector('.flipped');
-        const secondCard = clickedCard;
+
+    // Ensure you can only flip cards that are not already flipped or matched
+    if (clickedCard.classList.contains('flipped') || clickedCard.classList.contains('matched')) {
+        return;
+    }
+
+    clickedCard.classList.add('flipped');  // Flip the clicked card
+    clickedCard.innerText = clickedCard.dataset.card;  // Display the card's content
+
+    flippedCards.push(clickedCard);
+
+    if (flippedCards.length === 2) {
+        const [firstCard, secondCard] = flippedCards;
+
         if (firstCard.dataset.card === secondCard.dataset.card) {
             firstCard.classList.add('matched');
             secondCard.classList.add('matched');
         } else {
+            // If cards don't match, flip them back after a short delay
             setTimeout(() => {
                 firstCard.classList.remove('flipped');
                 secondCard.classList.remove('flipped');
+                firstCard.innerText = '';
+                secondCard.innerText = '';
             }, 1000);
         }
+
+        flippedCards = [];  // Reset the flipped cards
     }
 }
 
